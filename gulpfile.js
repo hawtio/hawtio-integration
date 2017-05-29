f43f4dfd37c70eac965115ee531cf26492147a22
Antonin Stefanutti <antonin@stefanutti.fr>
2017-05-29 11:27:17 +0200

    Activate ng-annotate to support minification

--- gulpfile.js
+++ gulpfile.js
@@ -65,6 +65,7 @@ gulp.task('tsc', ['clean-defs'], function() {
 
   return eventStream.merge(
     tsResult.js
+      .pipe(plugins.ngAnnotate())
       .pipe(plugins.if(config.sourceMap, plugins.sourcemaps.write()))
       .pipe(gulp.dest('.')),
     tsResult.dts
