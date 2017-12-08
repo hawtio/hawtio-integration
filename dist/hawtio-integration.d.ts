f97136bd170012287f384ecb9a7f73c2092fb395
Alexandre Kieling <abrianik@redhat.com>
2017-12-08 11:01:49 -0200

    use branding components in index.html

--- dist/hawtio-integration.d.ts
+++ dist/hawtio-integration.d.ts
@@ -30,77 +30,6 @@ declare namespace ActiveMQ {
     function decorate($scope: any, fn?: any): void;
     function getBrokerMBean(workspace: Jmx.Workspace, jolokia: any, jmxDomain: string): any;
 }
-declare namespace ActiveMQ {
-    class DestinationController {
-        private $scope;
-        private workspace;
-        private $location;
-        private jolokia;
-        private localStorage;
-        private log;
-        private readonly buttonNameLimit;
-        amqJmxDomain: any;
-        message: string;
-        destinationName: string;
-        destinationType: string;
-        createDialog: boolean;
-        deleteDialog: boolean;
-        purgeDialog: boolean;
-        constructor($scope: any, workspace: Jmx.Workspace, $location: ng.ILocationService, jolokia: Jolokia.IJolokia, localStorage: Storage);
-        private operationSuccess();
-        private deleteSuccess();
-        private validateDestinationName(name);
-        private isQueue(destinationType);
-        private checkIfDestinationExists(name, destinationType);
-        validateAndCreateDestination(name: string, destinationType: string): void;
-        private createDestination(name, destinationType);
-        /**
-         * When destination name contains "_" like "aaa_bbb", the actual name might be either
-         * "aaa_bbb" or "aaa:bbb", so the actual name needs to be checked before removal.
-         * @param name destination name
-         */
-        private restoreRealDestinationName(name);
-        deleteDestination(): void;
-        purgeDestination(): void;
-        selectedShortName(): string;
-        selectedName(): string;
-        uncapitalisedDestinationType(): string;
-    }
-}
-declare namespace ActiveMQ {
-    const createDestinationComponent: angular.IComponentOptions;
-    const deleteQueueComponent: angular.IComponentOptions;
-    const deleteTopicComponent: angular.IComponentOptions;
-}
-declare namespace ActiveMQ {
-    const destinationModule: string;
-}
-declare namespace ActiveMQ {
-    const pluginName: string;
-    const _module: angular.IModule;
-    function getBroker(workspace: Jmx.Workspace): Jmx.Folder;
-    function isQueue(workspace: Jmx.Workspace): boolean;
-    function isTopic(workspace: Jmx.Workspace): boolean;
-    function isQueuesFolder(workspace: Jmx.Workspace): boolean;
-    function isTopicsFolder(workspace: Jmx.Workspace): boolean;
-    function isJobScheduler(workspace: Jmx.Workspace): boolean;
-    function isBroker(workspace: Jmx.Workspace): boolean;
-}
-declare namespace ActiveMQ {
-    var BrowseQueueController: angular.IModule;
-}
-declare module ActiveMQ {
-}
-declare namespace ActiveMQ {
-}
-declare namespace ActiveMQ {
-}
-declare namespace ActiveMQ {
-}
-declare namespace ActiveMQ {
-}
-declare namespace ActiveMQ {
-}
 declare namespace Camel {
     /**
      * Define the default categories for endpoints and map them to endpoint names
@@ -1114,6 +1043,77 @@ declare namespace Camel {
 declare namespace Camel {
 }
 declare namespace Camel {
+}
+declare namespace ActiveMQ {
+    class DestinationController {
+        private $scope;
+        private workspace;
+        private $location;
+        private jolokia;
+        private localStorage;
+        private log;
+        private readonly buttonNameLimit;
+        amqJmxDomain: any;
+        message: string;
+        destinationName: string;
+        destinationType: string;
+        createDialog: boolean;
+        deleteDialog: boolean;
+        purgeDialog: boolean;
+        constructor($scope: any, workspace: Jmx.Workspace, $location: ng.ILocationService, jolokia: Jolokia.IJolokia, localStorage: Storage);
+        private operationSuccess();
+        private deleteSuccess();
+        private validateDestinationName(name);
+        private isQueue(destinationType);
+        private checkIfDestinationExists(name, destinationType);
+        validateAndCreateDestination(name: string, destinationType: string): void;
+        private createDestination(name, destinationType);
+        /**
+         * When destination name contains "_" like "aaa_bbb", the actual name might be either
+         * "aaa_bbb" or "aaa:bbb", so the actual name needs to be checked before removal.
+         * @param name destination name
+         */
+        private restoreRealDestinationName(name);
+        deleteDestination(): void;
+        purgeDestination(): void;
+        selectedShortName(): string;
+        selectedName(): string;
+        uncapitalisedDestinationType(): string;
+    }
+}
+declare namespace ActiveMQ {
+    const createDestinationComponent: angular.IComponentOptions;
+    const deleteQueueComponent: angular.IComponentOptions;
+    const deleteTopicComponent: angular.IComponentOptions;
+}
+declare namespace ActiveMQ {
+    const destinationModule: string;
+}
+declare namespace ActiveMQ {
+    const pluginName: string;
+    const _module: angular.IModule;
+    function getBroker(workspace: Jmx.Workspace): Jmx.Folder;
+    function isQueue(workspace: Jmx.Workspace): boolean;
+    function isTopic(workspace: Jmx.Workspace): boolean;
+    function isQueuesFolder(workspace: Jmx.Workspace): boolean;
+    function isTopicsFolder(workspace: Jmx.Workspace): boolean;
+    function isJobScheduler(workspace: Jmx.Workspace): boolean;
+    function isBroker(workspace: Jmx.Workspace): boolean;
+}
+declare namespace ActiveMQ {
+    var BrowseQueueController: angular.IModule;
+}
+declare module ActiveMQ {
+}
+declare namespace ActiveMQ {
+}
+declare namespace ActiveMQ {
+}
+declare namespace ActiveMQ {
+}
+declare namespace ActiveMQ {
+}
+declare namespace ActiveMQ {
 }
 declare namespace Karaf {
     var log: Logging.Logger;
