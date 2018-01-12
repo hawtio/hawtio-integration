b23aa0783073d4692ea97d5154287ef845c781f7
Alexandre Kieling <abrianik@redhat.com>
2018-01-12 09:34:25 -0200

    upgraded @hawtio/core and @hawtio/jmx dependencies

--- plugins/spring-boot/layout/layout.controller.ts
+++ plugins/spring-boot/layout/layout.controller.ts
@@ -3,14 +3,11 @@ namespace SpringBoot {
   export function SpringBootLayoutController($location: ng.ILocationService) {
     'ngInject';
 
-    const tabs = [
-      {name: 'Health', path: '/spring-boot/health'}
+    this.tabs = [
+      new Core.HawtioTab('Health', '/spring-boot/health')
     ];
 
-    this.names = tabs.map(tab => tab.name);
-
-    this.changePath = name => {
-      const tab = _.find(tabs, {name: name});
+    this.goto = tab => {
       $location.path(tab.path);
     };
   }
