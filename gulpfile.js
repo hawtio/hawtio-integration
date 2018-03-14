30681c762e009843054db959bb92f42a6ee4a811
Antonin Stefanutti <antonin@stefanutti.fr>
2018-03-14 10:51:45 +0100

    Remove site bundling tasks

--- gulpfile.js
+++ gulpfile.js
@@ -78,7 +78,7 @@ gulp.task('concat', ['template'], function() {
 });
 
 gulp.task('clean', ['concat'], function() {
-  del(['templates.js', 'compiled.js', './site/']);
+  del(['templates.js', 'compiled.js']);
 });
 
 gulp.task('less', function () {
@@ -161,109 +161,6 @@ gulp.task('reload', function() {
     .pipe(hawtio.reload());
 });
 
-gulp.task('site-fonts', () =>
-  gulp
-    .src(
-      [
-        'node_modules/**/*.woff',
-        'node_modules/**/*.woff2',
-        'node_modules/**/*.ttf',
-        'node_modules/**/fonts/*.eot',
-        'node_modules/**/fonts/*.svg'
-      ],
-      { base: '.' }
-    )
-    .pipe(plugins.flatten())
-    .pipe(plugins.chmod(0o644))
-    .pipe(plugins.dedupe({ same: false }))
-    .pipe(plugins.debug({ title: 'site font files' }))
-    .pipe(gulp.dest('docker/site/fonts/', { overwrite: false }))
-);
-
-gulp.task('site-files', () => gulp.src(['images/**', 'img/**'], { base: '.' })
-  .pipe(plugins.chmod(0o644))
-  .pipe(plugins.dedupe({ same: false }))
-  .pipe(plugins.debug({ title: 'site files' }))
-  .pipe(gulp.dest('docker/site')));
-
-gulp.task('site-usemin', () => gulp.src('index.html')
-  .pipe(plugins.usemin({
-    css: [plugins.minifyCss({ keepBreaks: true }), 'concat'],
-    js : [plugins.uglify(), plugins.rev()],
-  }))
-  .pipe(plugins.debug({ title: 'site usemin' }))
-  .pipe(gulp.dest('docker/site')));
-
-gulp.task('site-tweak-urls', ['site-usemin', 'site-config'], () => eventStream.merge(
-  gulp.src('docker/site/style.css')
-    .pipe(plugins.replace(/url\(\.\.\//g, 'url('))
-    // tweak fonts URL coming from PatternFly that does not repackage then in dist
-    .pipe(plugins.replace(/url\(\.\.\/components\/font-awesome\//g, 'url('))
-    .pipe(plugins.replace(/url\(\.\.\/components\/bootstrap\/dist\//g, 'url('))
-    .pipe(plugins.replace(/url\(node_modules\/bootstrap\/dist\//g, 'url('))
-    .pipe(plugins.replace(/url\(node_modules\/patternfly\/components\/bootstrap\/dist\//g, 'url('))
-    .pipe(gulp.dest('docker/site')),
-  gulp.src('docker/site/hawtconfig.json')
-    .pipe(plugins.replace(/node_modules\/@hawtio\/core\/dist\//g, ''))
-    .pipe(gulp.dest('docker/site')))
-  .pipe(plugins.debug({ title: 'site tweak urls' })));
-
-gulp.task('site-images', function () {
-  const dirs = fs.readdirSync('./node_modules/@hawtio');
-  const patterns = [];
-  dirs.forEach(function (dir) {
-    const path = './node_modules/@hawtio/' + dir + '/dist/img';
-    try {
-      if (fs.statSync(path).isDirectory()) {
-        console.log('found image dir: ', path);
-        const pattern = 'node_modules/@hawtio/' + dir + '/dist/img/**';
-        patterns.push(pattern);
-      }
-    } catch (e) {
-      // ignore, file does not exist
-    }
-  });
-  // Add PatternFly images package in dist
-  patterns.push('node_modules/patternfly/dist/img/**');
-  return gulp.src(patterns)
-    .pipe(plugins.debug({title: 'img-copy'}))
-    .pipe(plugins.chmod(0o644))
-    .pipe(gulp.dest('docker/site/img'));
-});
-
-gulp.task('site-config', () => gulp.src('hawtconfig.json')
-  .pipe(gulp.dest('docker/site')));
-
-gulp.task('serve-site', function () {
-  hawtio.setConfig({
-    port: 2772,
-    proxy: '/integration/proxy',
-    staticProxies: [{
-      port: config.proxyPort,
-      path: '/integration/jolokia',
-      targetPath: config.targetPath,
-    }],
-    staticAssets: [{
-      path : '/integration',
-      dir  : 'docker/site',
-    }],
-    fallback: 'docker/site/index.html',
-    liveReload : {
-      enabled : false,
-    },
-  });
-
-  hawtio.use('/', function(req, res, next) {
-    if (!req.originalUrl.startsWith('/integration')) {
-      res.redirect('/integration');
-    } else {
-      next();
-    }
-  });
-
-  return hawtio.listen(server => console.log(`Hawtio console started at http://localhost:${server.address().port}`));
-});
-
 gulp.task('test', ['build'], function (done) {
   new Server({
     configFile: __dirname + '/karma.conf.js'
@@ -271,7 +168,5 @@ gulp.task('test', ['build'], function (done) {
 });
 
 gulp.task('build', ['tsc', 'less', 'template', 'concat', 'clean', 'copy-images']);
-
-gulp.task('site', callback => sequence('clean', ['site-fonts', 'site-files', 'site-usemin', 'site-tweak-urls', 'site-images', 'site-config'], callback));
 
 gulp.task('default', ['connect']);
