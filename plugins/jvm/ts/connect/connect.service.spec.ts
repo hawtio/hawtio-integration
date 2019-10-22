a261712aa08e60f8f66af9d63fc943cf4c6f2682
Alexandre Kieling <alex.kieling@gmail.com>
2019-10-22 11:29:59 -0300

    fix(connect): type errors when connections are saved in Hawtio v1 format
    fix #87

--- plugins/jvm/ts/connect/connect.service.spec.ts
+++ plugins/jvm/ts/connect/connect.service.spec.ts
@@ -4,11 +4,42 @@ describe("ConnectService", function() {
 
   let connectService;
   let $location;
+  let $window;
 
   beforeEach(function() {
     $location = jasmine.createSpyObj('$location', ['absUrl', 'path']);
-    connectService = new JVM.ConnectService(null, null, $location);
+    $window = {localStorage: jasmine.createSpyObj('localStorage', ['getItem', 'setItem'])};
+    connectService = new JVM.ConnectService(null, $window, $location);
   });
+
+  describe("getConnections", function() {
+
+    it("should return saved connections", function () {
+      // given
+      const savedConnectionsJson = '[{"scheme":"http","host":"localhost","port":8181,"path":"/hawtio/jolokia","useProxy":true,"jolokiaUrl":null,"userName":"","password":"","view":null,"name":"karaf","reachable":false},{"name":"spring-boot","scheme":"http","host":"localhost","port":8080,"path":"/actuator/jolokia","useProxy":true,"jolokiaUrl":null,"userName":null,"password":null,"reachable":false}]';
+      const expectedConnections = JSON.parse(savedConnectionsJson);
+      $window.localStorage.getItem.and.returnValue(savedConnectionsJson);
+      // when
+      const connections = connectService.getConnections();
+      // then
+      expect($window.localStorage.getItem).toHaveBeenCalledWith('jvmConnect');
+      expect(connections).toEqual(expectedConnections);
+    });
+
+    it("should convert old connections", function () {
+      // given
+      const savedConnectionsJson = '{"karaf":{"scheme":"http","host":"localhost","port":8181,"path":"/hawtio/jolokia","useProxy":true,"jolokiaUrl":null,"view":null,"name":"karaf","userName":"admin","password":"admin"},"spring-boot":{"scheme":"http","host":"localhost","port":8080,"path":"/actuator/jolokia","useProxy":true,"jolokiaUrl":null,"view":null,"name":"spring-boot"}}';
+      const expectedConnections = Object.values(JSON.parse(savedConnectionsJson));
+      $window.localStorage.getItem.and.returnValue(savedConnectionsJson);
+      // when
+      const connections = connectService.getConnections();
+      // then
+      expect($window.localStorage.getItem).toHaveBeenCalledWith('jvmConnect');
+      expect(connections).toEqual(expectedConnections);
+    });
+
+  });
+
 
   describe("getBrowserUrlPortNumber", function() {
 
