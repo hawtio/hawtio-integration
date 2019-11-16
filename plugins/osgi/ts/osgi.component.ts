f223d54d75863220905f10d5dcc4e55545569b7c
Alexandre Kieling <alex.kieling@gmail.com>
2019-11-16 08:13:36 -0300

    fix(osgi): hide Server tab when the MBean that provides its data isn't available

--- plugins/osgi/ts/osgi.component.ts
+++ plugins/osgi/ts/osgi.component.ts
@@ -1,31 +1,34 @@
 namespace Osgi {
 
   export class OsgiController {
-    
+
     tabs: Nav.HawtioTab[];
 
     constructor(private workspace: Jmx.Workspace) {
       'ngInject';
     }
-    
+
     $onInit() {
       this.tabs = [];
 
       const bundlesTab = new Nav.HawtioTab('Bundles', '/osgi/bundles');
       this.tabs.push(bundlesTab);
-      
+
       if (Karaf.getSelectionFeaturesMBean(this.workspace)) {
         this.tabs.push(new Nav.HawtioTab('Features', '/osgi/features'));
       }
-      
+
       this.tabs.push(new Nav.HawtioTab('Packages', '/osgi/packages'));
       this.tabs.push(new Nav.HawtioTab('Services', '/osgi/services'));
-      
+
       if (Karaf.getSelectionScrMBean(this.workspace)) {
         this.tabs.push(new Nav.HawtioTab('Declarative Services', '/osgi/scr-components'));
       }
-      
-      this.tabs.push(new Nav.HawtioTab('Server', '/osgi/server'));
+
+      if (this.workspace.treeContainsDomainAndProperties('org.apache.karaf', { type: 'instance' })) {
+        this.tabs.push(new Nav.HawtioTab('Server', '/osgi/server'));
+      }
+
       this.tabs.push(new Nav.HawtioTab('Framework', '/osgi/fwk'));
       this.tabs.push(new Nav.HawtioTab('Configuration', '/osgi/configurations'));
     }
