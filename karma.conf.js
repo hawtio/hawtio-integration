4109005358d5f9e14308c9fec3aeabb00783eea4
Alexandre Kieling <alex.kieling@gmail.com>
2020-02-11 20:55:29 -0300

    chore: run tests using Firefox due to issues with Chrome

--- karma.conf.js
+++ karma.conf.js
@@ -93,7 +93,7 @@ module.exports = function(config) {
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['ChromeHeadless'],
+    browsers: ['FirefoxHeadless'],
 
 
     // Continuous Integration mode
