hawtio/hawtio-integration

commit 7a511d305819a062f65d3fccb6dc23241e36c32d

fix: show hawtio-confirm-dialog replace any modal-body

plugins/components/ui/ts/confirmDialogDirective.ts
@@ -130,7 +130,7 @@ namespace UI {
       $scope.$watch('show', function() {
         if ($scope.show) {
           setTimeout(function() {
-            $scope.body = $('.modal-body');
+            $scope.body = $element.find('.modal-body');
             $scope.body.html($compile($scope.clone.html())($scope.$parent));
             Core.$apply($scope);
           }, 50);

commit f95be0ac21fc0799e0817ab2e69e00f5187966db

fix: show hawtio-confirm-dialog broken by #110

plugins/components/ui/html/confirmDialog.html
@@ -1,5 +1,5 @@
 <div modal="show">
-  <div class="modal-dialog {{sizeClass}}">
+  <div class="hawtio-ui-confirm-dialog modal-dialog {{sizeClass}}">
     <div class="modal-content">    
       <div class="modal-header">
         <button type="button" class="close" aria-hidden="true" ng-click="cancel()">

plugins/components/ui/ts/confirmDialogDirective.ts
@@ -130,7 +130,7 @@ namespace UI {
       $scope.$watch('show', function() {
         if ($scope.show) {
           setTimeout(function() {
-            $scope.body = $element.find('.modal-body');
+            $scope.body = $('.hawtio-ui-confirm-dialog .modal-body');
             $scope.body.html($compile($scope.clone.html())($scope.$parent));
             Core.$apply($scope);
           }, 50);