7c109f2d54b64d7ad724dc65832a50625f3d2493
prahaladhchandrahasan <chandrahasanprahaladh@yahoo.com>
2022-05-30 13:56:41 +0530

    fix: created .less file

--- plugins/runtime/threads/thread-dump-modal.component.ts
+++ plugins/runtime/threads/thread-dump-modal.component.ts
@@ -23,7 +23,7 @@ namespace Runtime {
       <div class="modal-body">
         <div class="row">
           <div class="col-md-12">
-            <pre style="height:80vh">{{$ctrl.dumpedThreads}}</pre>
+              <pre class = "thread-dump-modal-output" >{{$ctrl.dumpedThreads}}</pre>
           </div>
         </div>
       </div>
